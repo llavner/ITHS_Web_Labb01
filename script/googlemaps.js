21517b26aa7d804ae1a558a7538df2cc8f86176a
llavner <marcus.renvall@gmail.com>
2025-02-25 10:07:52 +0100

    Github pages

--- script/googlemaps.js
+++ script/googlemaps.js
@@ -2,8 +2,6 @@ var map = document.getElementById ('mymap');
 var maplistner = function (e) { 
     var frame = document.createElement ('iframe'); 
     frame.src = this.getAttribute ('data-src');
-    frame.width = 200;
-    frame.height = 150;
     map.appendChild (frame); 
     map.removeEventListener ("mouseover", maplistner); 
 }; 
